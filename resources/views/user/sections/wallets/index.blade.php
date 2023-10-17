952667648bdde0bf88bd0726936721590484024c
Amir2417 <amirsohelto@gmail.com>
2023-10-17 11:47:09 +0600

    :user_panel(Wallet page added in the dashboard)

--- resources/views/user/sections/wallets/index.blade.php
+++ resources/views/user/sections/wallets/index.blade.php
@@ -14,12 +14,14 @@
 @endsection
 
 @section('content')
+<div class="body-wrapper">
     <div class="dashboard-area mt-10">
         @include('user.components.wallets.fiat',compact("fiat_wallets"))
     </div>
     <div class="dashboard-area mt-20">
         @include('user.components.wallets.crypto',compact("crypto_wallets"))
     </div>
+</div>
 @endsection
 
 @push('script')
